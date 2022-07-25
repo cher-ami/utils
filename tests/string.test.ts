082d11649696e106b2e9481e758c9f0510f48164
Willy Brauner <mail@willybrauner.com>
2022-07-25 16:47:15 +0200

    Update mergeClass to merge strings

--- tests/string.test.ts
+++ tests/string.test.ts
@@ -1,10 +1,10 @@
 import { describe, expect, it } from "vitest"
-import { mergeClass } from "../src/string"
+import { mergeStrings } from "../src/string"
 
 describe("mergeClass", () => {
   it("should be merge string and remove values", () => {
     const test = false
-    const result = mergeClass("a", !!test && "b", "c", "d", false ? "e" : null)
+    const result = mergeStrings(["a", !!test && "b", "c", "d", false ? "e" : null])
     expect(result).toBe("a c d")
   })
 })
