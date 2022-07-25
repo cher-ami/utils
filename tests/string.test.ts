c16d54bb4407f553d69a53e7e8569771488fcef5
Willy Brauner <mail@willybrauner.com>
2022-07-25 16:49:36 +0200

    Should return undefined

--- tests/string.test.ts
+++ tests/string.test.ts
@@ -2,9 +2,14 @@ import { describe, expect, it } from "vitest"
 import { mergeStrings } from "../src/string"
 
 describe("mergeClass", () => {
-  it("should be merge string and remove values", () => {
+  it("should be merge strings and remove values", () => {
     const test = false
     const result = mergeStrings(["a", !!test && "b", "c", "d", false ? "e" : null])
     expect(result).toBe("a c d")
+  })
+
+  it("should return undefined", () => {
+    const result = mergeStrings([])
+    expect(result).toBeUndefined()
   })
 })
