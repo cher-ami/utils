70982612a6899107467f17a6bbe09871ee63747f
Willy Brauner <mail@willybrauner.com>
2022-10-13 11:07:03 +0200

    Update isHandleDevice as function (#3)

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,7 @@
 /**
  * is handheld device check
  */
-export const isHandheldDevice: boolean =
+export const isHandheldDevice = () =>
   /(tablet|ipad|playbook|silk)|(android(?!.*mobi))|Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/i.test(
     navigator.userAgent
   )
