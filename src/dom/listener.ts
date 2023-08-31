aae752a0c01342b3a15aa23332b104d69962722b
Willy Brauner <willybrauner@gmail.com>
2023-08-31 15:47:27 +0200

    Create listener

---
+++ src/dom/listener.ts
@@ -0,0 +1,46 @@
+/**
+ * Listen to an event on an obj
+ * @param obj
+ * @param event
+ * @param callback
+ * @param options
+ */
+
+export function listen<K extends keyof (HTMLElementEventMap | DocumentEventMap | WindowEventMap)>(
+  obj: HTMLElement | Window | Document,
+  event: (K | string) | (K | string)[],
+  callback: (event: (HTMLElementEventMap | DocumentEventMap | WindowEventMap)[K]) => any,
+  options?: boolean | AddEventListenerOptions
+): () => void {
+  if (typeof event === "string") event = [event]
+  for (let name of event) obj.addEventListener(name, callback, options)
+  return () => {
+    for (let name of event) obj.removeEventListener(name, callback, options)
+  }
+}
+
+export function listenOnce<
+  K extends keyof (HTMLElementEventMap | DocumentEventMap | WindowEventMap)
+>(
+  obj: HTMLElement | Window | Document,
+  event: (K | string) | (K | string)[],
+  callback: (event: (HTMLElementEventMap | DocumentEventMap | WindowEventMap)[K]) => any,
+  options?: boolean | AddEventListenerOptions
+): () => void {
+  const unlisten = listen(
+    obj,
+    event,
+    (event) => {
+      unlisten()
+      callback(event)
+    },
+    options
+  )
+  return unlisten
+}
+
+export function listenCompose(...listeners: (() => void)[]): () => void {
+  return () => {
+    for (let listener of listeners) listener()
+  }
+}
