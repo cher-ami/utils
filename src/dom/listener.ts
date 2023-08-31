2a42ea67247557ac86fed8a9e7ae507a849913dc
Willy Brauner <willybrauner@gmail.com>
2023-08-31 15:58:27 +0200

    Add doc

--- src/dom/listener.ts
+++ src/dom/listener.ts
@@ -1,16 +1,26 @@
 /**
  * Listen to an event on an obj
+ *
+ * ex:
+ *    const unlisten = listen(window, "resize", () => {})
+ *
+ *    unlisten()
+ *
  * @param obj
  * @param event
  * @param callback
  * @param options
  */
 
-export function listen<K extends keyof (HTMLElementEventMap | DocumentEventMap | WindowEventMap)>(
+export function listen<
+  K extends keyof (HTMLElementEventMap | DocumentEventMap | WindowEventMap),
+>(
   obj: HTMLElement | Window | Document,
   event: (K | string) | (K | string)[],
-  callback: (event: (HTMLElementEventMap | DocumentEventMap | WindowEventMap)[K]) => any,
-  options?: boolean | AddEventListenerOptions
+  callback: (
+    event: (HTMLElementEventMap | DocumentEventMap | WindowEventMap)[K],
+  ) => any,
+  options?: boolean | AddEventListenerOptions,
 ): () => void {
   if (typeof event === "string") event = [event]
   for (let name of event) obj.addEventListener(name, callback, options)
@@ -19,13 +29,27 @@ export function listen<K extends keyof (HTMLElementEventMap | DocumentEventMap |
   }
 }
 
+/**
+ * Listen to an event on an obj, and remove the listener after the first call
+ *
+ * ex:
+ *
+ *    listenOnce(window, "resize", () => {})
+ *
+ * @param obj
+ * @param event
+ * @param callback
+ * @param options
+ */
 export function listenOnce<
-  K extends keyof (HTMLElementEventMap | DocumentEventMap | WindowEventMap)
+  K extends keyof (HTMLElementEventMap | DocumentEventMap | WindowEventMap),
 >(
   obj: HTMLElement | Window | Document,
   event: (K | string) | (K | string)[],
-  callback: (event: (HTMLElementEventMap | DocumentEventMap | WindowEventMap)[K]) => any,
-  options?: boolean | AddEventListenerOptions
+  callback: (
+    event: (HTMLElementEventMap | DocumentEventMap | WindowEventMap)[K],
+  ) => any,
+  options?: boolean | AddEventListenerOptions,
 ): () => void {
   const unlisten = listen(
     obj,
@@ -34,11 +58,25 @@ export function listenOnce<
       unlisten()
       callback(event)
     },
-    options
+    options,
   )
   return unlisten
 }
 
+/**
+ * Compose listeners
+ *
+ * ex:
+ *
+ *    const unlisten = listenCompose(
+ *      listen(window, "resize", () => {})
+ *      listen(window, "scroll", () => {})
+ *    )
+ *
+ *    unlisten()
+ *
+ * @param listeners
+ */
 export function listenCompose(...listeners: (() => void)[]): () => void {
   return () => {
     for (let listener of listeners) listener()
